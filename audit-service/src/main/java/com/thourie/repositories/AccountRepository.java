15be85005613402ce774e764fd6777fb048f7b6f
thourie <wargellar@proton.me>
2024-11-22 15:02:42 +0300

    Add Account Repository

---
+++ audit-service/src/main/java/com/thourie/repositories/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.thourie.repositories;
+
+import com.thourie.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+}
