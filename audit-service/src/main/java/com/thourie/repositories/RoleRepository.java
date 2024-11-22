d2b8d870d6a0d3689f17415343430f36fd28f66a
thourie <wargellar@proton.me>
2024-11-22 15:33:44 +0300

    Edit Account and Role Repository

--- audit-service/src/main/java/com/thourie/repositories/RoleRepository.java
+++ audit-service/src/main/java/com/thourie/repositories/RoleRepository.java
@@ -3,7 +3,9 @@ package com.thourie.repositories;
 
 import com.thourie.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
